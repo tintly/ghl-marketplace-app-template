1c7cdc9c7dd866c0de0326fcdafd77cfc54c2ffe
tintly <tintly.io@gmail.com>
2025-06-27 09:34:48 -0400

    Setup full-stack deployment with real authentication

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
