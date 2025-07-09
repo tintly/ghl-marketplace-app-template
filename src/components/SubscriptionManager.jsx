b3e179c7f5bb3f025e43618bf482712348cf2b66
tintly <tintly.io@gmail.com>
2025-07-09 10:40:53 -0400

    Update Subscription Plan Layout

--- src/components/SubscriptionManager.jsx
+++ src/components/SubscriptionManager.jsx
@@ -283,7 +283,7 @@ function UpgradePlanModal({ currentPlan, availablePlans, onUpgrade, onCancel }) 
           </div>
 
           {/* Plan Selection */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {upgradePlans.map(plan => (
               <div 
                 key={plan.id}
